9bb7dc00ce4d7f1729639614a55b0388bb27e65f
AleEirea97 <63886280+AleEirea97@users.noreply.github.com>
2020-09-06 19:27:13 -0300

    Update main.c

--- Código/main.c
+++ Código/main.c
@@ -3,18 +3,23 @@
 int main(){
 
 	bspi_t master[1] = {};		//MASTER
-	bspi_t slaves[3] = {};		//SLAVES.
+	bspi_t slaves[3] = {};	//SLAVES.
  	resp_st_t status = {};
 
-	init_t(&master,status,  &slaves);		//MASTER configurado.
+	init_t(&master,status,  &slaves);	//MASTER configurado.
 
 	while(1){
  		switch (status.estado){
-        	case MOSI:	status = f_MOSI(status, &master);		break;
-					case SDI:		status = f_SDI(status, &master,  &slaves);	break;
-       	 	case SDO: 	status = f_SDO(status, &master,  &slaves);	break;
-        	case MISO: 	status = f_MISO(status,  &master);	break;
+        	case MOSI:	status = f_MOSI(status, &master);
+											break;
+					case SDI:		status = f_SDI(status, &master,  &slaves);
+            					break;
+       	 	case SDO:  	status = f_SDO(status, &master,  &slaves);
+											break;
+        	case MISO: 	status = f_MISO(status,  &master);
+											break;
       	}
 	}
-	return 0;
+
+	 return 0;
 }
